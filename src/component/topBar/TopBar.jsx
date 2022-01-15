f0f1b064f8d627d96d2734f22c38a0dc6452864b
mohammadkabbara <mohammadkabbara40@gmail.com>
2022-01-15 21:35:37 +0200

    half done

--- src/component/topBar/TopBar.jsx
+++ src/component/topBar/TopBar.jsx
@@ -9,12 +9,24 @@ export default function TopBar() {
                 <a href="#Intro" className="logo">
             genius.
           </a>       
+
+          <div className="itemContainer">
+         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png" alt="" className="icon" />
+
+         <span>00962785902342</span>
+          </div>
+
+          <div className="itemContainer">
+         <img src="https://www.creativefabrica.com/wp-content/uploads/2019/04/Mail-icon-by-ahlangraphic-55-580x386.jpg" alt="" className="icon"/>
+
+         <span>mohammadkabbara40@gmail.com</span>
+          </div>
                 </div>
 
         
                 <div className="right">
 
-                    hiii 
+                   
                 </div>
             </div>
         </div>
