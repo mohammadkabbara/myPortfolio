889b36bfd1a1ebaf22fe2ce5af68f322a9bc6b37
mohammadkabbara <mohammadkabbara40@gmail.com>
2022-01-18 22:51:23 +0200

    topBar done

---
+++ src/component/menu/Menu.jsx
@@ -0,0 +1,37 @@
+import "./Menu.scss";
+
+export default function Menu({ menuOpen, setMenuOpen }) {
+    return (
+        <div className={"menu "  + (menuOpen && "active")}>
+            <ul>
+
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#Intro">Home</a>
+                </li>
+
+
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#contact">Contact</a>
+                </li>
+
+
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#protifol">Protifolio</a>
+                </li>
+
+
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#Testimoinals">Testimoinals</a>
+                </li>
+
+
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#work">Works</a>
+                </li>
+
+
+            </ul>
+            
+        </div>
+    )
+}
