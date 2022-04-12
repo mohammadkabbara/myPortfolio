75a99cc494fad505e277702398926150273c7153
mohammadkabbara <mohammadkabbara40@gmail.com>
2022-04-12 04:19:14 +0300

    done80%

---
+++ src/data.js
@@ -0,0 +1,22 @@
+import Sun from "./img/wallhaven.jpg";
+export const products = [
+    {
+      id: 1,
+      img :{Sun},
+      link: "http://lama.dev",
+      des:"good place",
+    },
+    {
+      id: 2,
+      src :{Sun},
+      link: "http://lama.dev",
+      des:"good place",
+    },
+    {
+      id: 3,
+      src :{Sun},
+      link: "http://lama.dev",
+      des:"good place",
+    },
+    
+  ];
